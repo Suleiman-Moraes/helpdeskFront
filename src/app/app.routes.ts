4b86cca01912d2692df6cee0b5699e890927fc6d
Suleiman Moraes <suleimanmoraes@gmail.com>
2019-01-23 09:46:12 -0200

    Cadastro novo User

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { UserNewComponent } from './components/user-new/user-new.component';
 import { LoginComponent } from './components/security/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { Routes, RouterModule } from '@angular/router';
@@ -7,6 +8,7 @@ import { AuthGuard } from './components/security/auth.guard';
 export const ROUTES: Routes = [
     { path: '', component: HomeComponent, canActivate: [AuthGuard]},
     { path: 'login', component: LoginComponent },
+    { path: 'user-new', component: UserNewComponent, canActivate: [AuthGuard] }
 ]
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(ROUTES);
